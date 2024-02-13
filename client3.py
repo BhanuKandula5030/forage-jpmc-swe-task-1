e7ef6a12e0470e751dfb11db5d4cdd546500bd6c
BhanuKandula5030 <k.bhannuprasad@gmail.com>
2024-02-12 18:24:48 -0600

    Modified the getRatio,getDatapoints and main method to get teh right values of Ratio and added the unit test cases

--- client3.py
+++ client3.py
@@ -28,32 +28,39 @@ QUERY = "http://localhost:8080/query?id={}"
 # 500 server request
 N = 500
 
+prices = {}
 
 def getDataPoint(quote):
-    """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
+    """Produce all the needed values to generate a datapoint."""
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2
+    prices[stock] = price
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
-    """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    """Get the ratio of price_a and price_b."""
+    if price_b == 0:
+        return None
+    return price_a / price_b
 
 
 # Main
 if __name__ == "__main__":
-    # Query the price once every N seconds.
-    for _ in iter(range(N)):
+    for _ in range(N):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
+        data_points = []
 
-        """ ----------- Update to get the ratio --------------- """
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            data_points.append((stock, price))
+        for i in range(len(data_points)):
+            for j in range(i+1, len(data_points)):
+                stock_a, price_a = data_points[i]
+                stock_b, price_b = data_points[j]
+                ratio = getRatio(prices[stock_a], prices[stock_b])
+                print("Ratio %s/%s: %s" % (stock_a, stock_b, ratio))
 
-        print("Ratio %s" % getRatio(price, price))
--- client_test.py
+++ client_test.py
@@ -2,24 +2,49 @@ import unittest
 from client3 import getDataPoint
 
 class ClientTest(unittest.TestCase):
-  def test_getDataPoint_calculatePrice(self):
-    quotes = [
-      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
+    def test_getDataPoint_calculatePrice(self):
+        quotes = [
+            {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+            {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
+        expected_data_points = [('ABC', 120.48, 121.2, (120.48 + 121.2) / 2),
+                                ('DEF', 117.87, 121.68, (117.87 + 121.68) / 2)]
 
-  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
-    quotes = [
-      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
+        for quote, expected_data_point in zip(quotes, expected_data_points):
+            self.assertEqual(getDataPoint(quote), expected_data_point)
 
+    def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+        quotes = [
+            {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+            {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
+        expected_data_points = [('ABC', 120.48, 119.2, (120.48 + 119.2) / 2),
+                                ('DEF', 117.87, 121.68, (117.87 + 121.68) / 2)]
 
-  """ ------------ Add more unit tests ------------ """
+        for quote, expected_data_point in zip(quotes, expected_data_points):
+            self.assertEqual(getDataPoint(quote), expected_data_point)
 
+    def test_getDataPoint_zeroBidPrice(self):
+        quotes = [
+            {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 0, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+            {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
+        expected_data_points = [('ABC', 0, 121.2, 121.2 / 2),
+                                ('DEF', 117.87, 121.68, (117.87 + 121.68) / 2)]
 
+        for quote, expected_data_point in zip(quotes, expected_data_points):
+            self.assertEqual(getDataPoint(quote), expected_data_point)
+
+    def test_getDataPoint_zeroAskPrice(self):
+        quotes = [
+            {'top_ask': {'price': 0, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+            {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
+        expected_data_points = [('ABC', 120.48, 0, 120.48 / 2),
+                                ('DEF', 117.87, 121.68, (117.87 + 121.68) / 2)]
+
+        for quote, expected_data_point in zip(quotes, expected_data_points):
+            self.assertEqual(getDataPoint(quote), expected_data_point)
 
 if __name__ == '__main__':
     unittest.main()
